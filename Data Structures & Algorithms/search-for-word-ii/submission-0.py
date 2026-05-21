b037f1a55ecd7e9c31481bc7e03a83b84ea6a0d7
簡柏松 <83742453+Benson0409@users.noreply.github.com>
2026-05-21 21:49:45 +0800

    Add: search-for-word-ii - submission-0

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.py
@@ -0,0 +1,54 @@
+
+class TrieNode:
+    def __init__(self):
+        self.node = defaultdict(TrieNode)
+        self.word = None
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        
+        root = TrieNode()
+        for word in words:
+            current_node = root
+            for char in word:
+                current_node = current_node.node[char]
+            current_node.word = word
+
+        answer = []
+        rows = len(board)
+        cols = len(board[0])
+
+
+        def dfs(r, c, current_node):
+            
+            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] == "#":
+                return
+            
+            char = board[r][c]
+            
+            if char not in current_node.node:
+                return
+            
+            next_node = current_node.node[char]
+            
+            if next_node.word != None:
+                answer.append(next_node.word)
+                next_node.word = None 
+
+            board[r][c] = "#"
+            
+            dfs(r + 1, c, next_node)
+            dfs(r - 1, c, next_node)
+            dfs(r, c + 1, next_node)
+            dfs(r, c - 1, next_node)
+            
+            board[r][c] = char
+
+        for i in range(rows):
+            for j in range(cols):
+                dfs(i, j, root)
+
+        return answer
+
+
+        
