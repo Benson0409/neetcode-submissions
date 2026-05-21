960f137afe910cc6f3fb494537bba85edf247c2e
簡柏松 <83742453+Benson0409@users.noreply.github.com>
2026-05-21 13:56:48 +0800

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,39 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.node = defaultdict(PrefixTree)
+        self.isEnd = False
+
+    def insert(self, word: str) -> None:
+        current_node = self
+        for i in word:
+            current_node = current_node.node[i]
+
+        current_node.isEnd = True
+
+    def search(self, word: str) -> bool:
+        current_node = self
+        for i in word:
+
+            if i not in current_node.node:
+                return False
+            
+            current_node = current_node.node[i]
+
+            if i == word[-1] and current_node.isEnd:
+                return True
+
+        return False
+            
+
+    def startsWith(self, prefix: str) -> bool:
+        current_node = self
+        
+        for i in prefix:
+
+            if i not in current_node.node:
+                return False
+            current_node = current_node.node[i]
+
+        return True
+        
