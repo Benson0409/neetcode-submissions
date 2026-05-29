406c1c44268563dc0c21a7862633c762798bc707
簡柏松 <83742453+Benson0409@users.noreply.github.com>
2026-05-29 12:01:41 +0800

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        course_count = numCourses*[0]
+        course = defaultdict(list)
+        answer = []
+
+        for second,first in prerequisites:
+            course[first].append(second)
+            course_count[second] += 1
+
+        bus = deque()
+
+        for i in range(len(course_count)):
+            if course_count[i] == 0:
+                bus.append(i)
+            
+
+        while bus:
+            current = bus.popleft()
+            answer.append(current)
+            for i in course[current]:
+                course_count[i] -= 1
+                if course_count[i] == 0:
+                    bus.append(i)
+
+        if numCourses != len(answer):
+                return []
+        return answer
