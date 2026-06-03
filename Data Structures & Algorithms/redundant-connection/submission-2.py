f0b2105e8f8027fa3391958809c83e50b7495948
簡柏松 <83742453+Benson0409@users.noreply.github.com>
2026-06-03 23:04:41 +0800

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        count = len(edges)
+
+        parent = []
+        for i in range(count+1):
+            parent.append(i)
+
+        def find(i):
+            if parent[i] == i:
+                return i
+
+            parent[i] = find(parent[i])
+            return parent[i]
+
+        for a,b in edges:
+            root_a = find(a)
+            root_b = find(b)
+
+            if root_a == root_b:
+                return [a,b]
+
+            parent[root_a] = root_b
+
+        return []
